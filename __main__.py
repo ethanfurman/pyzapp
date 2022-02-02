10bc8cef1780e46d1398286a93d9a26440722871
Ethan Furman <ethan@stoneleaf.us>
2022-02-01 20:24:02 -0800

    always use temp dir, copy includes directly

--- __main__.py
+++ __main__.py
@@ -56,35 +56,69 @@ def pyzapp(source, output, include, shebang, compress, force):
     compression = compress and ZIP_DEFLATED or ZIP_STORED
     print('compress: %r' % compression)
     #
-    # ensure we have a directory bundle for source
-    if source.isdir():
-        remove_src = False
-        source_dir = source
-    else:
-        remove_src = True
-        source_dir = Path(mkdtemp())
-        source.copy(source_dir/'main.py')
-    print('working dir: %r' % source_dir)
-    #
+    # there are three possibilities:
+    # - converting a single-script app into a pyzapp
+    # - converting a directory bundle consisting of a __main__.py and maybe a test.py into a pyzapp
+    # - converting a python package with a __main__.py, __init__.py, etc., into a pyzapp
     # create __main__ if missing
+    if source.exists('__main__.py') and source.exists('__init__.py'):
+        # option 3
+        mode = 'package'
+        print('mode =', mode)
+        pass
+    elif source.exists('__init__.py'):
+        abort('unable to convert a package with no __main__.py')
+    elif source.isdir():
+        if not source.exists('__main__.py'):
+            abort('directory conversions must have a __main__.py as one will not be created')
+        mode = 'subdir'
+        print('mode =', mode)
+        pass
+    else:
+        mode = 'script'
+        print('mode =', mode)
+        # must have at least on of them
+    #
+    # ensure we have a directory bundle for source
+    source_dir = Path(mkdtemp())
+    module_name = None
+    if mode == 'package':
+        module_name = source.filename
+        source.copytree(source_dir/module_name)
+    elif mode == 'subdir':
+        source_dir.rmdir()
+        source.copytree(source_dir)
+    else:
+        source.copy(source_dir/'cli.py')
+    print('working dir: %r' % source_dir)
+
     if not source_dir.exists('__main__.py'):
-        if not source_dir.exists('main.py'):
-            abort('main.py or __main__.py is required')
         included = source_dir.glob()
         new_main = []
         new_main.append('import sys')
-        new_main.append('if sys.path[0] == ".":')
-        new_main.append('    sys.path.pop(0)\n')
+        # new_main.append('if sys.path[0] == ".":')
+        # new_main.append('    sys.path.pop(0)\n')
         new_main.append('')
         new_main.append('# protect against different versions of modules being imported')
         new_main.append("# by Python's startup procedures")
         new_main.append('saved_modules = []')
+        imports = []
         for inc in included:
             if inc.ext == '.py' or inc.isdir():
                 new_main.append('saved_modules.append(sys.modules.pop("%s", None))' % inc.stem)
-        new_main.append('\nimport main')
+                imports.append(inc.stem)
+        for inc in include:
+            new_main.append('saved_modules.append(sys.modules.pop("%s", None))' % inc)
+            imports.append(inc)
+        if imports:
+            new_main.append('import %s' % ', '.join(imports))
+        new_main.append('')
+        if mode == 'script':
+            new_main.append('import cli')
+        elif mode == 'package':
+            new_main.append('from %s import __main__' % module_name)
         with open(source_dir/'__main__.py', 'w') as m:
-            m.write('\n'.join(new_main))
+            m.write('\n'.join(new_main) + '\n')
     #
     # create the archive and add source files
     with open(output, 'wb') as fd:
@@ -114,6 +148,7 @@ def pyzapp(source, output, include, shebang, compress, force):
                     module = sys.modules[module_name]
                 else:
                     module = __import__(module_name)
+                print('using %s from %r' % (module_name, module.__file__))
                 module_dir = Path(module.__file__).dirname
                 for dirpath, dirnames, filenames in module_dir.walk():
                     if '.git' in dirnames:
@@ -130,8 +165,7 @@ def pyzapp(source, output, include, shebang, compress, force):
                         print('adding %r as %r' % (f, arcname))
                         zf.write(f, arcname=arcname)
     output.chmod(0o555)
-    if remove_src:
-        source_dir.rmtree()
+    source_dir.rmtree()
 
 
 # helpers
